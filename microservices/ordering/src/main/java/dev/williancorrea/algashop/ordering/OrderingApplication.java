d4a7df6c4cde60eb0cf1da4b4adb4beb3695129e
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2026-01-03 18:03:36 -0300

    Fix indentation and clean up `.gitignore`
    - Standardize tab usage in `OrderingApplication` and test class.
    - Remove redundant `.log` entry from `.gitignore`.

--- microservices/ordering/src/main/java/dev/williancorrea/algashop/ordering/OrderingApplication.java
+++ microservices/ordering/src/main/java/dev/williancorrea/algashop/ordering/OrderingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrderingApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(OrderingApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingApplication.class, args);
+	}
 
 }
