04d42e83f40b6ecefc073f058f8ad9f75a155569
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2026-01-03 18:03:09 -0300

    Add initial implementation for the "ordering" microservice
    Includes:
    - Spring Boot setup with Java 21
    - Gradle build configuration
    - Basic application structure with `application.yaml`
    - Unit test scaffold

---
+++ microservices/ordering/src/test/java/dev/williancorrea/algashop/ordering/OrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.williancorrea.algashop.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
