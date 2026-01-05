166646e2ee034148451ae7fcb2c3118a41217a11
Willian Vagner Vicente Corrêa <willian.vag@gmail.com>
2026-01-05 00:43:31 -0300

    Refactor `Customer` entity to include validation and private setters; update dependencies for validation and testing

--- microservices/ordering/src/main/java/dev/williancorrea/algashop/ordering/domain/entity/Customer.java
+++ microservices/ordering/src/main/java/dev/williancorrea/algashop/ordering/domain/entity/Customer.java
@@ -4,14 +4,13 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.UUID;
-import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
-import lombok.Setter;
+import org.apache.commons.validator.routines.EmailValidator;
 
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Setter(AccessLevel.PRIVATE)
 public class Customer implements Serializable {
 
   @Serial
@@ -28,18 +27,20 @@ public class Customer implements Serializable {
   private Boolean archived;
   private OffsetDateTime registeredAt;
   private OffsetDateTime archivedAt;
-  private int loyaltyPoints;
+  private Integer loyaltyPoints;
 
   public Customer(UUID id, String fullName, LocalDate birthDate, String email, String phone, String document,
                   Boolean promotionNotificationsAllowed, OffsetDateTime registeredAt) {
-    this.id = id;
-    this.fullName = fullName;
-    this.birthDate = birthDate;
-    this.email = email;
-    this.phone = phone;
-    this.document = document;
-    this.promotionNotificationsAllowed = promotionNotificationsAllowed;
-    this.registeredAt = registeredAt;
+    this.setId(id);
+    this.setFullName(fullName);
+    this.setBirthDate(birthDate);
+    this.setEmail(email);
+    this.setPhone(phone);
+    this.setDocument(document);
+    this.setPromotionNotificationsAllowed(promotionNotificationsAllowed);
+    this.setRegisteredAt(registeredAt);
+    this.setArchived(false);
+    this.setLoyaltyPoints(0);
   }
 
   public Customer(UUID id,
@@ -53,17 +54,17 @@ public class Customer implements Serializable {
                   OffsetDateTime registeredAt,
                   OffsetDateTime archivedAt,
                   Integer loyaltyPoints) {
-    this.id = id;
-    this.fullName = fullName;
-    this.birthDate = birthDate;
-    this.email = email;
-    this.phone = phone;
-    this.document = document;
-    this.promotionNotificationsAllowed = promotionNotificationsAllowed;
-    this.archived = archived;
-    this.registeredAt = registeredAt;
-    this.archivedAt = archivedAt;
-    this.loyaltyPoints = loyaltyPoints;
+    this.setId(id);
+    this.setFullName(fullName);
+    this.setBirthDate(birthDate);
+    this.setEmail(email);
+    this.setPhone(phone);
+    this.setDocument(document);
+    this.setPromotionNotificationsAllowed(promotionNotificationsAllowed);
+    this.setArchived(archived);
+    this.setRegisteredAt(registeredAt);
+    this.setArchivedAt(archivedAt);
+    this.setLoyaltyPoints(loyaltyPoints);
   }
 
   public void addLoyaltyPoints(Integer points) {
@@ -137,5 +138,76 @@ public class Customer implements Serializable {
 
   public Integer loyaltyPoints() {
     return loyaltyPoints;
+  }
+
+  private void setId(UUID id) {
+    Objects.requireNonNull(id);
+    this.id = id;
+  }
+
+  private void setFullName(String fullName) {
+    Objects.requireNonNull(fullName);
+    if (fullName.isBlank()) {
+      throw new IllegalArgumentException("Full name cannot be blank");
+    }
+    this.fullName = fullName;
+  }
+
+  private void setBirthDate(LocalDate birthDate) {
+    if (birthDate == null) {
+      this.birthDate = null;
+      return;
+    }
+
+    if (birthDate.isAfter(LocalDate.now())) {
+      throw new IllegalArgumentException("Birth date cannot be in the future");
+    }
+
+    this.birthDate = birthDate;
+  }
+
+  private void setEmail(String email) {
+    Objects.requireNonNull(email);
+    if (email.isBlank()) {
+      throw new IllegalArgumentException("Email cannot be blank");
+    }
+    if (!EmailValidator.getInstance().isValid(email)) {
+      throw new IllegalArgumentException("Invalid email");
+    }
+    this.email = email;
+  }
+
+  private void setPhone(String phone) {
+    Objects.requireNonNull(phone);
+    this.phone = phone;
+  }
+
+  private void setDocument(String document) {
+    Objects.requireNonNull(document);
+    this.document = document;
+  }
+
+  private void setPromotionNotificationsAllowed(Boolean promotionNotificationsAllowed) {
+    Objects.requireNonNull(promotionNotificationsAllowed);
+    this.promotionNotificationsAllowed = promotionNotificationsAllowed;
+  }
+
+  private void setArchived(Boolean archived) {
+    Objects.requireNonNull(archived);
+    this.archived = archived;
+  }
+
+  private void setRegisteredAt(OffsetDateTime registeredAt) {
+    Objects.requireNonNull(registeredAt);
+    this.registeredAt = registeredAt;
+  }
+
+  private void setArchivedAt(OffsetDateTime archivedAt) {
+    this.archivedAt = archivedAt;
+  }
+
+  private void setLoyaltyPoints(Integer loyaltyPoints) {
+    Objects.requireNonNull(loyaltyPoints);
+    this.loyaltyPoints = loyaltyPoints;
   }
 }
